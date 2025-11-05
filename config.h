7b4fcc7bf56bcc4eb257df5ae33efa898589695f
aaravlu <demolemon@outlook.at>
2025-11-05 09:40:29 +0800

    Update config.h

--- config.h
+++ config.h
@@ -19,12 +19,15 @@ static int instant = 0;                     /* -n  option; if 1, selects matchin
 #endif // INSTANT_PATCH
 #if CENTER_PATCH
 static int center = 1;                      /* -c  option; if 0, dmenu won't be centered on the screen */
-static int min_width = 500;                 /* minimum width when centered */
+static int min_width = 1;                 /* minimum width when centered */
 #endif // CENTER_PATCH
 #if BARPADDING_PATCH
 static const int vertpad = 10;              /* vertical padding of bar */
 static const int sidepad = 10;              /* horizontal padding of bar */
 #endif // BARPADDING_PATCH
+#if QUIET_PATCH
+static int quiet = 0;                       /* -q  option; if 1, dmenu will not show any items if the search string is empty */
+#endif // QUIET_PATCH
 #if RESTRICT_RETURN_PATCH
 static int restrict_return = 0;             /* -1 option; if 1, disables shift-return and ctrl-return */
 #endif // RESTRICT_RETURN_PATCH
@@ -38,7 +41,7 @@ static char *fonts[] =
 static const char *fonts[] =
 #endif // XRESOURCES_PATCH
 {
-    "Source Han Serif SC:size=18"
+    "Source Han Serif SC:size=20"
 };
 #endif // PANGO_PATCH
 #if MANAGED_PATCH
@@ -55,8 +58,8 @@ static const char *symbol_2 = ">";
 #endif // SYMBOLS_PATCH
 
 #if ALPHA_PATCH
-static const unsigned int baralpha = 0xaa;
-static const unsigned int borderalpha = 0xff;
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
@@ -97,14 +100,14 @@ const
 #endif // XRESOURCES_PATCH
 char *colors[][2] = {
 	/*               fg         bg       */
-	[SchemeNorm] = { "#efefef", "#000000" },
-	[SchemeSel]  = { "#ef9f9f", "#000000" },
-	[SchemeOut]  = { "#000000", "#000000" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 	#if BORDER_PATCH
-	// [SchemeBorder] = { "#000000", "#005577" },
+	[SchemeBorder] = { "#000000", "#005577" },
 	#endif // BORDER_PATCH
 	#if MORECOLOR_PATCH
-	// [SchemeMid]  = { "#eeeeee", "#770000" },
+	[SchemeMid]  = { "#eeeeee", "#770000" },
 	#endif // MORECOLOR_PATCH
 	#if HIGHLIGHT_PATCH
 	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
